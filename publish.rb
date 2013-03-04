21de4366764965174feec7c8ee72265623fd6c1d
froderik <fredrik@eldfluga.se>
2013-03-04 20:37:08 +0100

    updated after input from nanoc guy

--- publish.rb
+++ publish.rb
@@ -3,19 +3,5 @@
 require 'tmpdir'
 
 `git checkout master`
-`rm -rf output`
-`nanoc`
-Dir.mktmpdir { |dir|
-  `cp -r output/* #{dir}`
-  target_branch = 'gh-pages'
-  if system( "git checkout #{target_branch}" )
-    `rm -rf *`
-    `cp -r #{dir}/* .`
-    `git add .`
-    `git commit -m "high level bits automatic deploy"`
-    `git push git@github.com:highlevelbits/blog.git #{target_branch}`
-    `git checkout master`
-  else
-    p "failed to checkout #{target_branch} - maybe there is unfinished work on master?"
-  end
-}
+`nanoc prune compile`
+`git push git@github.com:highlevelbits/blog.git gh-pages`
