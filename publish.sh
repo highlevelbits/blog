c8971cdadf6068fc23569fb62c97095c4b09fd56
froderik <fredrik@eldfluga.se>
2013-03-04 20:43:16 +0100

    minor

--- publish.sh
+++ publish.sh
@@ -2,7 +2,8 @@
 
 nanoc prune compile --yes
 cd output
-git commit -m "high level bits automatic deploy"
+git add .
+git commit -am "high level bits automatic deploy"
 git push git@github.com:highlevelbits/blog.git gh-pages
-cd ..`
+cd ..
 
