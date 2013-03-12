94f4fdacc5c96d1c0f80306eb166b27fd81255c7
froderik <fredrik@eldfluga.se>
2013-03-12 22:08:41 +0100

    raw archive without looks.....

--- lib/default.rb
+++ lib/default.rb
@@ -3,3 +3,51 @@
 
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
+
+class Month
+  attr_accessor :items
+  attr_accessor :name
+
+  def initialize month_number
+    self.items = []
+    self.name = Date::MONTHNAMES[month_number]
+  end
+
+  def count
+    self.items.size
+  end
+
+  def add item
+    self.items << item
+  end
+end
+
+class Year
+  attr_accessor :months
+
+  def initialize
+    self.months = {}
+  end
+
+  def count
+    self.months.values.map( &:count ).reduce( :+ )
+  end
+
+  def add item
+    item_month = item[:created_at].month
+    self.months[item_month] ||= Month.new( item_month )
+    self.months[item_month].add item
+  end
+
+  def self.articles_per_year sorted_articles
+    years = {}
+
+    sorted_articles.each do |item|
+      item_year = item[:created_at].year
+      years[item_year] ||= Year.new
+      years[item_year].add item
+    end
+
+    years
+  end
+end
