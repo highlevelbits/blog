fc3aa015fd70dbcee81c1988f9a930f09846bd0e
froderik <fredrik@eldfluga.se>
2013-03-24 21:21:11 +0100

    adds untagged page

--- lib/tags.rb
+++ lib/tags.rb
@@ -1,4 +1,11 @@
 
+class Object
+  def present?
+    understands_empty = self.respond_to?( :empty? )
+    not self.nil? and (understands_empty ? self.empty? : true)
+  end
+end
+
 def tags_for item
   if item[:tags]
     item[:tags].split(',').map {|tag| tag.strip }
@@ -12,5 +19,9 @@ def items_tagged_with tag, except = []
   other_items.select do |i|
     (i[:tags] || []).include?(tag)
   end
+end
+
+def items_without_tags
+  @items.select { |i| not i[:tags].present? }
 end
 
