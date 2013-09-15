7f7c136fccd023fc1d8499875693869357bc2b9e
froderik <fredrik@eldfluga.se>
2013-09-15 14:38:05 +0200

    don't repeat yourself

--- lib/tags.rb
+++ lib/tags.rb
@@ -17,3 +17,11 @@ def items_without_tags
   @items.select { |i| tags_for(i).empty? }
 end
 
+def tags_with_sibling_articles_for item
+  tags_for( @item ).each do |one_tag|
+    similar_articles = items_tagged_with one_tag, [@item]
+    if similar_articles.any?
+      yield one_tag, similar_articles
+    end
+  end
+end
