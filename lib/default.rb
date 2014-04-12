bc94e692ffbb13cccc7fd748850335700ec34cb2
Fredrik Rubensson <fredrik@eldfluga.se>
2014-04-12 08:00:25 +0200

    reuses markup for rendering an item row in a post list

--- lib/default.rb
+++ lib/default.rb
@@ -1,9 +1,28 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
+require 'haml'
+
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
 
 def bypass? extension
   ['css','svg'].include? extension
+end
+
+def short article
+  markup = 
+"""
+.row.article-entry
+  %code
+    = article[:created_at].strftime '%F'
+  = '-'
+  %b
+    = link_to article[:title], article
+  by
+  = article[:author]
+"""
+
+  engine = Haml::Engine.new markup
+  engine.render self, article: article 
 end
