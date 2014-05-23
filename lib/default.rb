19d0d664274895b97c6b83d1afa6b9cc4947c33b
Fredrik Rubensson <fredrik@eldfluga.se>
2014-05-23 07:59:05 +0200

    tags in article lists - made an effort to reuse tag listing but failed.....

--- lib/default.rb
+++ lib/default.rb
@@ -14,13 +14,22 @@ def short article
   markup = 
 """
 .row.article-entry
-  %code
-    = article[:created_at].strftime '%F'
-  = '-'
-  %b
-    = link_to article[:title], article
-  by
-  = article[:author]
+  %span.description
+    %code
+      = article[:created_at].strftime '%F'
+    = '-'
+    %b
+      = link_to article[:title], article
+    by
+    = article[:author]
+  .tags
+    - tags_for( article ).each do |one_tag|
+      - similarly_tagged = items_tagged_with( one_tag )
+      - similarly_tagged.delete(article)
+      - if similarly_tagged.any?
+        %a.one-tag{ href: link_to_tag( one_tag ) }
+          %span.icon-tag
+            = one_tag
 """
 
   engine = Haml::Engine.new markup
